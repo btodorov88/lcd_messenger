79489c1ab55cbf97cd58ad8568ae294f6a9f6277
btodorov <btodorov88@gmail.com>
2015-05-03 11:14:03 +0200

    messenger is now reusable module

--- Messenger.py
+++ Messenger.py
@@ -2,19 +2,24 @@ import time
 import LCD_Ext as Ext
 from multiprocessing import Pool, Queue, Manager
 
-light_pool = Pool(1)
-beeper_pool = Pool(1)
-msg_pool = Pool(1)
+def init():
+    global light_pool, beeper_pool, msg_queue
+    light_pool = Pool(1)
+    beeper_pool = Pool(1)
+    msg_pool = Pool(1)
 
-q = Manager().Queue()
-msg_pool.apply_async(Ext.printMsg, [q]) 
+    msg_queue = Manager().Queue()
+    msg_pool.apply_async(Ext.printMsg, [msg_queue]) 
 
-messages = ["Hi!", "Kak si?", "Kakvo shte kaje6? ", "Chao!"]
-
-for x in messages:
+def send_message(msg): 
     light_pool.apply_async(Ext.light, [9]) 
     beeper_pool.apply_async(Ext.beep, []) 
-    q.put(x);
-    
-    time.sleep(10)
+    msg_queue.put(msg);
+
+if __name__ == '__main__':
+    messages = ["Hi!", "Kak si?", "Kakvo shte kaje6? ", "Chao!"]
+    init()
+    for x in messages:
+        send_message(x)
+        time.sleep(10)
     
